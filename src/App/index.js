d8b19e85824448ea1a4ecb16c6099e6a031a6600
unknown <juanpabloferroarroyo@gmail.com>
2021-11-28 20:52:11 +0100

    creating a custom hook for managing the localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -9,15 +9,39 @@ paleta de colores
 .color5 {color: #55e7ff;}
 */
 
+/*
 const defaultTodos = [
   { text: "Take a course with Platzi", completed: true },
   { text: "Set a meeting for tomorrow", completed: false },
   { text: "Do the shopping for the week", completed: false },
   { text: "Workout for 2-3 hours", completed: false },
 ];
+*/
+
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
 
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = useState("");
 
   const completedTodos = todos.filter((x) => !!x.completed).length;
@@ -38,13 +62,13 @@ function App() {
     const todoIndex = todos.findIndex((t) => t.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
   const deleteTodos = (text) => {
     const todoIndex = todos.findIndex((t) => t.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
